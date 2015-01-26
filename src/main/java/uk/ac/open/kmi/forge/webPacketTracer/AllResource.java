06bb0bb09d7b8c986fe66355042c0d12e63858c0
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-01-26 14:42:53 +0000

    Returning /all resources by getting their POJOs (i.e., using MOXy).

--- src/main/java/uk/ac/open/kmi/forge/webPacketTracer/AllResource.java
+++ src/main/java/uk/ac/open/kmi/forge/webPacketTracer/AllResource.java
@@ -1,9 +1,25 @@
 package uk.ac.open.kmi.forge.webPacketTracer;
 
+import sun.nio.ch.Net;
+import uk.ac.open.kmi.forge.webPacketTracer.gateway.PTCallable;
+import uk.ac.open.kmi.forge.webPacketTracer.pojo.Device;
+import uk.ac.open.kmi.forge.webPacketTracer.pojo.Network;
+
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+
+class AllGetter extends PTCallable<Network> {
+    @Override
+    public Network internalRun() {
+        final com.cisco.pt.ipc.sim.Network network = this.task.getIPC().network();
+        return Network.fromCiscoObject(network);
+    }
+}
 
 @Path("all")
 public class AllResource {
@@ -12,7 +28,11 @@ public class AllResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public String getJson() {
+    public Network getAll() {
+        return new AllGetter().call();  // No Threads
+    }
+
+    public String getJson2() {
         if (ptSmith == null) {
             ptSmith = new PtSmith();
         }
--- src/main/java/uk/ac/open/kmi/forge/webPacketTracer/EdgesResource.java
+++ src/main/java/uk/ac/open/kmi/forge/webPacketTracer/EdgesResource.java
@@ -1,10 +1,10 @@
 package uk.ac.open.kmi.forge.webPacketTracer;
 
-import com.cisco.pt.ipc.sim.Network;
 import com.cisco.pt.ipc.sim.port.Link;
 import com.cisco.pt.ipc.sim.port.Port;
 import uk.ac.open.kmi.forge.webPacketTracer.gateway.PTCallable;
 import uk.ac.open.kmi.forge.webPacketTracer.pojo.Edge;
+import uk.ac.open.kmi.forge.webPacketTracer.pojo.Network;
 
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
@@ -19,25 +19,10 @@ class EdgesGetter extends PTCallable<Collection<Edge>> {
 
     @Override
     public Collection<Edge> internalRun() {
-        final Network network = this.task.getIPC().network();
-        final Map<String, Edge> ret = new HashMap<String, Edge>();
-        for (int i = 0; i < network.getDeviceCount(); i++) {
-            com.cisco.pt.ipc.sim.Device d = network.getDeviceAt(i);
-            for (int j = 0; j < d.getPortCount(); j++) {
-                final Port port = d.getPortAt(j);
-                final Link currentLink = port.getLink();
-                if (currentLink != null) {
-                    final String linkId = currentLink.getObjectUUID().getDecoratedHexString();
-                    final String devId = d.getObjectUUID().getDecoratedHexString();
-                    if (ret.containsKey(linkId)) {
-                        ret.get(linkId).setTo(devId);
-                    } else {
-                        ret.put(linkId, new Edge(linkId, devId, null));
-                    }
-                }
-            }
-        }
-        return ret.values();
+        final com.cisco.pt.ipc.sim.Network network = this.task.getIPC().network();
+        // Even if fromCiscoObject() returns more data than the one needed here,
+        // it does not go over additional loops.
+        return Network.fromCiscoObject(network).getEdges();
     }
 }
 
