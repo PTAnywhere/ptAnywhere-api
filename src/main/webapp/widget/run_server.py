737ea07532b03cc2c7f476d6a9d801ea24e5a65f
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-02-23 12:49:37 +0000

    Adding python script to serve the widget page outside the Java app. This will make faster to test frontend changes (JS GUI) as the Java app won't be built and deployed after each GUI change.

---
+++ src/main/webapp/widget/run_server.py
@@ -0,0 +1,15 @@
+# Simple HTTP server to test JS side of the widget.
+# Take into account that the webapi should run in a CORS enabled server as the pages from this server will be in a different port.
+# In tomcat7: http://tomcat.apache.org/tomcat-7.0-doc/config/filter.html#CORS_Filter
+
+import SimpleHTTPServer
+import SocketServer
+
+PORT = 8000
+
+Handler = SimpleHTTPServer.SimpleHTTPRequestHandler
+
+httpd = SocketServer.TCPServer(("", PORT), Handler)
+
+print "serving at port", PORT
+httpd.serve_forever()
