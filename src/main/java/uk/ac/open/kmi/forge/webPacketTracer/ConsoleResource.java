755c490f608083ecbc23aa334fcd0a9f470149ec
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-02-25 17:26:33 +0000

    Adding a console resource to the API which returns an HTML representation.

---
+++ src/main/java/uk/ac/open/kmi/forge/webPacketTracer/ConsoleResource.java
@@ -0,0 +1,20 @@
+package uk.ac.open.kmi.forge.webPacketTracer;
+
+import javax.servlet.ServletContext;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import java.io.*;
+
+@Path("devices/{device}/console")
+public class ConsoleResource {
+
+    @Context
+    ServletContext ctx;
+
+    @GET
+    @Produces(MediaType.TEXT_HTML)
+    public InputStream getDevice(@PathParam("device") String deviceId) {
+        return this.ctx.getResourceAsStream("widget/console.html");
+    }
+}
