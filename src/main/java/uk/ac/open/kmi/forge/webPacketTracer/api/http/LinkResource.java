fda2d2e7ba9f56e129ef3d3db467cb62293ace05
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-02-27 17:52:12 +0000

    Adding links to HTTP header in /link

--- src/main/java/uk/ac/open/kmi/forge/webPacketTracer/api/http/LinkResource.java
+++ src/main/java/uk/ac/open/kmi/forge/webPacketTracer/api/http/LinkResource.java
@@ -8,7 +8,6 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.UriInfo;
-import java.net.URI;
 
 
 class LinkGetter extends  PTCallable<Link> {
@@ -24,41 +23,37 @@ class LinkGetter extends  PTCallable<Link> {
     }
 }
 
-class LinkDeleter extends PTCallable<Response> {
+class LinkDeleter extends PTCallable<Link> {
     final Link toDelete = new Link();
     public LinkDeleter(String deviceId, String portName) {
         this.toDelete.setToDevice(deviceId);
         this.toDelete.setToPort(portName);
     }
     @Override
-    public Response internalRun() {
+    public Link internalRun() {
         final boolean success = this.connection.getDataAccessObject().removeLink(this.toDelete.getToDevice(), this.toDelete.getToPort());
         if (success)
-            return Response.ok().entity(this.toDelete).build();
-        else
-            return Response.status(Response.Status.NOT_ACCEPTABLE).entity(this.toDelete).build();
+            return this.toDelete;
+        return null;
     }
 }
 
-class LinkCreator extends PTCallable<Response> {
+class LinkCreator extends PTCallable<Link> {
     final String deviceId;
     final String portName;
     final Link linkToCreate;
-    final URI requestUri;
-    public LinkCreator(String deviceId, String portName, Link linkToCreate, URI requestUri) {
+    public LinkCreator(String deviceId, String portName, Link linkToCreate) {
         this.deviceId = deviceId;
         this.portName = portName;
         this.linkToCreate = linkToCreate;
-        this.requestUri = requestUri;
     }
     @Override
-    public Response internalRun() {
+    public Link internalRun() {
         final boolean success = this.connection.getDataAccessObject().createLink(this.deviceId, this.portName, this.linkToCreate);
-        if (success) {
-            final Link link = this.connection.getDataAccessObject().getLink(this.deviceId, this.portName);
-            return Response.created(this.requestUri).entity(link).build();
-        } else
-            return Response.status(Response.Status.NOT_ACCEPTABLE).entity(this.linkToCreate).build();  // Improvable performance
+        if (success)
+            // Improvable performance
+            return this.connection.getDataAccessObject().getLink(this.deviceId, this.portName);
+        return null;
     }
 }
 
@@ -69,15 +64,26 @@ public class LinkResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Link getLink(@PathParam("device") String deviceId,
+    public Response getLink(@PathParam("device") String deviceId,
                           @PathParam("port") String portName) {
-        return new LinkGetter(deviceId, Utils.unescapePort(portName)).call();
+        final Link l = new LinkGetter(deviceId, Utils.unescapePort(portName)).call();
+        if (l==null)
+            return Response.noContent().
+                    links(getPortLink()).build();
+        return Response.ok(l).
+                links(getPortLink()).
+                links(getToPortLink(l)).build();
     }
     @DELETE
     @Produces(MediaType.APPLICATION_JSON)
     public Response removeLink(@PathParam("device") String deviceId,
                              @PathParam("port") String portName) {
-        return new LinkDeleter(deviceId, Utils.unescapePort(portName)).call();
+        final Link deletedLink = new LinkDeleter(deviceId, Utils.unescapePort(portName)).call();
+        if (deletedLink==null)
+            return Response.status(Response.Status.NOT_ACCEPTABLE).entity(deletedLink).
+                    links(getPortLink()).build();
+        return Response.ok(deletedLink).
+                links(getPortLink()).build();
     }
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
@@ -85,6 +91,22 @@ public class LinkResource {
     public Response createLink(Link newLink,
                              @PathParam("device") String deviceId,
                              @PathParam("port") String portName) {
-        return new LinkCreator(deviceId, Utils.unescapePort(portName), newLink, this.uri.getRequestUri()).call();
+        final Link createdLink = new LinkCreator(deviceId, Utils.unescapePort(portName), newLink).call();
+        if (createdLink==null)
+            return Response.status(Response.Status.NOT_ACCEPTABLE).entity(newLink).
+                    links(getPortLink()).build();
+        return Response.created(this.uri.getRequestUri()).entity(createdLink).
+                links(getPortLink()).
+                links(getToPortLink(createdLink)).build();
+    }
+    private javax.ws.rs.core.Link getPortLink() {
+        return javax.ws.rs.core.Link.fromUri(this.uri.getRequestUri().resolve("..")).rel("port").build();  // Rename it to from?
+    }
+    private javax.ws.rs.core.Link getToPortLink(Link l) {
+        return javax.ws.rs.core.Link.fromUri(
+                    this.uri.getBaseUri() +
+                    "devices/" + l.getToDevice() +
+                    "/ports/" + Utils.escapeIdentifier(l.getToPort()) + "?byName=true"
+                ).rel("to").build();  // Rename it to port?
     }
 }
