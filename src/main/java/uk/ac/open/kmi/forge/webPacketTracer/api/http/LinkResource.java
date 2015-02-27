f26a952727deaac93119a3cde1d94f4c7e5a34f0
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-02-27 13:13:09 +0000

    Returning HTTP 201 status code during the link creation.

--- src/main/java/uk/ac/open/kmi/forge/webPacketTracer/api/http/LinkResource.java
+++ src/main/java/uk/ac/open/kmi/forge/webPacketTracer/api/http/LinkResource.java
@@ -4,8 +4,11 @@ import uk.ac.open.kmi.forge.webPacketTracer.gateway.PTCallable;
 import uk.ac.open.kmi.forge.webPacketTracer.pojo.Link;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
 
 
 class LinkGetter extends  PTCallable<Link> {
@@ -41,23 +44,29 @@ class LinkCreator extends PTCallable<Response> {
     final String deviceId;
     final String portName;
     final Link linkToCreate;
-    public LinkCreator(String deviceId, String portName, Link linkToCreate) {
+    final URI requestUri;
+    public LinkCreator(String deviceId, String portName, Link linkToCreate, URI requestUri) {
         this.deviceId = deviceId;
         this.portName = portName;
         this.linkToCreate = linkToCreate;
+        this.requestUri = requestUri;
     }
     @Override
     public Response internalRun() {
         final boolean success = this.connection.getDataAccessObject().createLink(this.deviceId, this.portName, this.linkToCreate);
-        if (success)
-            return Response.ok().entity(this.connection.getDataAccessObject().getLink(this.deviceId, this.portName)).build();
-        else
+        if (success) {
+            final Link link = this.connection.getDataAccessObject().getLink(this.deviceId, this.portName);
+            return Response.created(this.requestUri).entity(link).build();
+        } else
             return Response.status(Response.Status.NOT_ACCEPTABLE).entity(this.linkToCreate).build();  // Improvable performance
     }
 }
 
 @Path("devices/{device}/ports/{port}/link")
 public class LinkResource {
+    @Context
+    UriInfo uri;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
     public Link getLink(@PathParam("device") String deviceId,
@@ -76,6 +85,6 @@ public class LinkResource {
     public Response createLink(Link newLink,
                              @PathParam("device") String deviceId,
                              @PathParam("port") String portName) {
-        return new LinkCreator(deviceId, Utils.unescapePort(portName), newLink).call();
+        return new LinkCreator(deviceId, Utils.unescapePort(portName), newLink, this.uri.getRequestUri()).call();
     }
 }
