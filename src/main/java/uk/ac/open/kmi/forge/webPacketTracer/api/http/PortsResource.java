f6090496ad485ad30b963359425d1ef63699fbe1
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-02-27 17:02:26 +0000

    Adding links to HTTP headers in /ports

--- src/main/java/uk/ac/open/kmi/forge/webPacketTracer/api/http/PortsResource.java
+++ src/main/java/uk/ac/open/kmi/forge/webPacketTracer/api/http/PortsResource.java
@@ -1,18 +1,20 @@
 package uk.ac.open.kmi.forge.webPacketTracer.api.http;
 
 import uk.ac.open.kmi.forge.webPacketTracer.gateway.PTCallable;
+import uk.ac.open.kmi.forge.webPacketTracer.pojo.Device;
 import uk.ac.open.kmi.forge.webPacketTracer.pojo.Port;
 
 import javax.ws.rs.*;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.*;
 import java.util.Collection;
+import java.util.Iterator;
 
 class PortsGetter extends PTCallable<Collection<Port>> {
     final String deviceId;
     final boolean byName;
-    public PortsGetter(String deviceId, boolean isName) {
+    public PortsGetter(String deviceId, boolean byName) {
         this.deviceId = deviceId;
-        this.byName = isName;
+        this.byName = byName;
     }
     @Override
     public Collection<Port> internalRun() {
@@ -22,11 +24,34 @@ class PortsGetter extends PTCallable<Collection<Port>> {
 
 @Path("devices/{device}/ports")
 public class PortsResource {
+    @Context UriInfo uri;
+
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Collection<Port> getPorts(
+    public Response getPorts(
             @PathParam("device") String deviceId,
             @DefaultValue("false") @QueryParam("byName") boolean byName) {
-        return new PortsGetter(deviceId, byName).call();  // Not using a new Thread
+        final Collection<Port> p = new PortsGetter(deviceId, byName).call();  // Not using a new Thread
+        // To array because otherwise Response does not know how to serialize Collection<Device>
+        return Response.ok(p.toArray(new Port[p.size()])).
+                links(getDeviceLink(deviceId)).
+                links(getPortLinks(p)).build();
+    }
+
+    private Link getDeviceLink(String deviceId) {
+        return Link.fromUri(this.uri.getBaseUri() + "devices/" + Utils.escapeIdentifier(deviceId)).rel("device").build();
+    }
+
+    private Link getPortLink(String portName) {
+        return Link.fromUri(this.uri.getRequestUri() + "/" + Utils.escapeIdentifier(portName)).rel("item").build();
+    }
+
+    private Link[] getPortLinks(Collection<Port> ports) {
+        final Link[] links = new Link[ports.size()];
+        final Iterator<Port> portIt = ports.iterator();
+        for(int i=0; i<links.length; i++) {
+            links[i] = getPortLink(portIt.next().getPortName());
+        }
+        return links;
     }
 }
