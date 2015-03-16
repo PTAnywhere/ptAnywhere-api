0ccc84036b5e8c59d121ffd7efde1b2bed9e3801
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-03-16 15:38:29 +0000

    Logging events on Console's webSocket endpoint.

--- src/main/java/uk/ac/open/kmi/forge/webPacketTracer/api/websocket/ConsoleEndpoint.java
+++ src/main/java/uk/ac/open/kmi/forge/webPacketTracer/api/websocket/ConsoleEndpoint.java
@@ -5,6 +5,8 @@ import com.cisco.pt.ipc.events.TerminalLineEventListener;
 import com.cisco.pt.ipc.events.TerminalLineEventRegistry;
 import com.cisco.pt.ipc.sim.Pc;
 import com.cisco.pt.ipc.sim.TerminalLine;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import uk.ac.open.kmi.forge.webPacketTracer.gateway.PTConnection;
 import java.io.IOException;
 import javax.websocket.*;
@@ -13,6 +15,7 @@ import javax.websocket.server.ServerEndpoint;
 @ServerEndpoint("/endpoint/devices/{device}/console")
 public class ConsoleEndpoint implements TerminalLineEventListener {
 
+    private static Log logger = LogFactory.getLog(TerminalLineEventListener.class);
     PTConnection common;
     TerminalLine cmd;
     Session session;
@@ -27,14 +30,21 @@ public class ConsoleEndpoint implements TerminalLineEventListener {
 
         this.common.open();
         final String deviceId = "{" + session.getPathParameters().get("device") + "}";
+        if (logger.isInfoEnabled()) {
+            logger.info("Opening communication channel for device " + deviceId + "'s command line.");
+        }
         final Pc pc0 = (Pc) this.common.getDataAccessObject().getSimDeviceById(deviceId);
-        this.cmd = pc0.getCommandLine();
-        try {
-            final TerminalLineEventRegistry registry = this.common.getTerminalLineEventRegistry();
-            this.session.getBasicRemote().sendText(this.cmd.getPrompt());
-            registry.addListener(this, this.cmd);
-        } catch(IOException e) {
-            this.common.getLog().error(e.getMessage(), e);
+        if (pc0 != null) {
+            this.cmd = pc0.getCommandLine();
+            try {
+                final TerminalLineEventRegistry registry = this.common.getTerminalLineEventRegistry();
+                this.session.getBasicRemote().sendText(this.cmd.getPrompt());
+                registry.addListener(this, this.cmd);
+            } catch (IOException e) {
+                logger.error(e.getMessage(), e);
+            }
+        } else if(logger.isErrorEnabled()) {
+            logger.error("Device with id " + deviceId + " not found." );
         }
     }
 
@@ -44,7 +54,7 @@ public class ConsoleEndpoint implements TerminalLineEventListener {
             //System.out.println("Closing a WebSocket due to " + reason.getReasonPhrase());
             this.common.getTerminalLineEventRegistry().removeListener(this, this.cmd);
         } catch(IOException e) {
-            this.common.getLog().error(e.getMessage(), e);
+            logger.error(e.getMessage(), e);
         } finally {
             this.common.close();
         }
@@ -63,7 +73,7 @@ public class ConsoleEndpoint implements TerminalLineEventListener {
                 final String msg = ((TerminalLineEvent.OutputWritten) event).newOutput;
                 this.session.getBasicRemote().sendText(msg);
             } catch(IOException e) {
-                this.common.getLog().error(e.getMessage(), e);
+                logger.error(e.getMessage(), e);
             }
         }
     }
