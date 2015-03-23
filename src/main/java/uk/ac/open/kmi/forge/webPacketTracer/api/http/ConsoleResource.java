01ca49e0e7ee8942cf558b901f4e3ed0d2734fb3
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-03-23 16:39:23 +0000

    Checking that the device has a command line interface and throwing HTTP error otherwise.

--- src/main/java/uk/ac/open/kmi/forge/webPacketTracer/api/http/ConsoleResource.java
+++ src/main/java/uk/ac/open/kmi/forge/webPacketTracer/api/http/ConsoleResource.java
@@ -1,10 +1,29 @@
 package uk.ac.open.kmi.forge.webPacketTracer.api.http;
 
+import com.cisco.pt.ipc.enums.DeviceType;
+import com.cisco.pt.ipc.sim.Device;
+import uk.ac.open.kmi.forge.webPacketTracer.gateway.PTCallable;
+
 import javax.servlet.ServletContext;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import java.io.*;
+
+
+class CommandLineGetter extends PTCallable<Boolean> {
+    final String dId;
+    public CommandLineGetter(String dId) {
+        this.dId = dId;
+    }
+    @Override
+    public Boolean internalRun() {
+        final Device d = this.connection.getDataAccessObject().getSimDeviceById(this.dId);
+        return d.getCommandLine()!=null; // if not null, it has a console.
+        /*if (DeviceType.PC.equals(d.getType()) || DeviceType.SWITCH.equals(d.getType()) ||
+                DeviceType.ROUTER.equals(d.getType())) {*/
+    }
+}
 
 @Path("devices/{device}/console")
 public class ConsoleResource {
@@ -12,9 +31,16 @@ public class ConsoleResource {
     @Context
     ServletContext ctx;
 
+    boolean deviceHasCommandLine(String deviceId) {
+        return new CommandLineGetter(deviceId).call();
+    }
+
     @GET
     @Produces(MediaType.TEXT_HTML)
     public InputStream getDevice(@PathParam("device") String deviceId) {
-        return this.ctx.getResourceAsStream("widget/console.html");
+        if (deviceHasCommandLine(deviceId))
+            return this.ctx.getResourceAsStream("widget/console.html");
+        else
+            throw new InternalServerErrorException("This device does not have command line.");
     }
 }
