ac01d086b6801606537dffe670543e9dfee7efdb
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-04-15 14:55:33 +0100

    Adding verb to delete session (i.e., release it).

--- src/main/java/uk/ac/open/kmi/forge/webPacketTracer/api/http/SessionResource.java
+++ src/main/java/uk/ac/open/kmi/forge/webPacketTracer/api/http/SessionResource.java
@@ -1,5 +1,7 @@
 package uk.ac.open.kmi.forge.webPacketTracer.api.http;
 
+import uk.ac.open.kmi.forge.webPacketTracer.pojo.Device;
+
 import javax.ws.rs.*;
 import javax.ws.rs.core.*;
 
@@ -11,13 +13,22 @@ public class SessionResource {
 
     @GET
     @Produces(MediaType.APPLICATION_JSON)
-    public Response getSession(
-            @PathParam("session") String sessionId) {
+    public Response getSession(@PathParam("session") String sessionId) {
         final String requestUri = Utils.getURIWithSlashRemovingQuery(this.uri.getRequestUri());
         return Response.ok("Sample return").
                 links(getSessionsLink()).
                 link(requestUri + "devices", "devices").
                 link(requestUri + "network", "network").build();
+    }
+
+    @DELETE
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response removeDevice(@PathParam("session") String sessionId) {
+        if (!sessionId.equals("id4"))
+            return Response.noContent().
+                    links(getSessionsLink()).build();
+        return Response.ok(). // TODO return deleted session.
+                links(getSessionsLink()).build();
     }
 
     private Link getSessionsLink() {
