ece3a4fad9a8d1bfe2721b2ff9e35fab6b207a44
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-07-27 17:55:24 +0100

    Application-level executor is now handled by APIApplication object.

--- src/main/java/uk/ac/open/kmi/forge/webPacketTracer/analytics/InteractionRecordFactory.java
+++ src/main/java/uk/ac/open/kmi/forge/webPacketTracer/analytics/InteractionRecordFactory.java
@@ -2,9 +2,6 @@ package uk.ac.open.kmi.forge.webPacketTracer.analytics;
 
 import java.net.MalformedURLException;
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.ThreadFactory;
-
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import uk.ac.open.kmi.forge.webPacketTracer.properties.InteractionRecordingProperties;
@@ -15,52 +12,23 @@ public class InteractionRecordFactory {
 
     private static final Log LOGGER = LogFactory.getLog(InteractionRecordFactory.class);
 
-    // Right we only use it to ensure that the TinCanAPI clients execute in their own threads.
-    // But if in the future this executor does other type of tasks,
-    // we should consider moving its management to the Application object.
-    private ExecutorService executor = null;
-
+    private final ExecutorService executor;  // This executor is not handled by this class.
     private final InteractionRecordingProperties irp;
 
-    public InteractionRecordFactory() {
+    public InteractionRecordFactory(ExecutorService executor) {
+        this.executor = executor;
         // Only an object is created per application, so we are not reading the file over and over again.
         final PropertyFileManager pfm = new PropertyFileManager();
         this.irp = pfm.getInteractionRecordingDetails();
     }
 
-    private ExecutorService getExecutor() {
-        // Lazy creation to prevent NoTracker record from unnecessarily creating it.
-        if (this.executor!=null) {
-            return this.executor;
-        }
-        // TODO adjust number of threads
-        this.executor = Executors.newFixedThreadPool(10, new SimpleDaemonFactory());
-        return this.executor;
-    }
-
     public InteractionRecord create() {
         if (this.irp==null) new NoTracker();
         try {
-            return new TinCanAPI(this.irp.getEndpoint(), this.irp.getUsername(), this.irp.getPassword(), getExecutor());
+            return new TinCanAPI(this.irp.getEndpoint(), this.irp.getUsername(), this.irp.getPassword(), this.executor);
         } catch(MalformedURLException e) {
             LOGGER.error(e.getMessage());
             return new NoTracker();
         }
-    }
-
-    public void shutdown() {
-        LOGGER.debug("Destroying InteractionRecordFactory.");
-        this.executor.shutdownNow();
-    }
-}
-
-
-// Following the advice from:
-//   http://stackoverflow.com/questions/3745905/what-is-recommended-way-for-spawning-threads-from-a-servlet-in-tomcat
-class SimpleDaemonFactory implements ThreadFactory {
-    public Thread newThread(Runnable r) {
-        final Thread t =new Thread(r);
-        t.setDaemon(true);
-        return t;
     }
 }
--- src/main/java/uk/ac/open/kmi/forge/webPacketTracer/api/http/APIApplication.java
+++ src/main/java/uk/ac/open/kmi/forge/webPacketTracer/api/http/APIApplication.java
@@ -10,6 +10,9 @@ import javax.annotation.PreDestroy;
 import javax.servlet.ServletContext;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Context;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
 
 @ApplicationPath("api")
@@ -18,12 +21,22 @@ public class APIApplication extends ResourceConfig {
     private static final String INTERACTION_RECORD_FACTORY = "interactionRecordFactory";
     private static final Log LOGGER = LogFactory.getLog(APIApplication.class);
 
+    // Executor used by the application for:
+    //   1. Ensuring that the TinCanAPI clients execute in their own threads.
+    //   2. Have a blocking Redis subscription object running during the application lifetime.
+    private final ExecutorService executor;
+
+    // FIXME not sure whether creating one PropertyFileManager per request could be harmful.
+    // Just in case, only an object is created per application so we are not reading the file over and over again.
     private final InteractionRecordFactory irf;
+
 
     public APIApplication(@Context ServletContext servletContext) {
         LOGGER.debug("Creating API webapp.");
         packages(getClass().getPackage().getName());
-        this.irf = new InteractionRecordFactory();
+
+        this.executor = Executors.newFixedThreadPool(10, new SimpleDaemonFactory());
+        this.irf = new InteractionRecordFactory(this.executor);
         servletContext.setAttribute(INTERACTION_RECORD_FACTORY, this.irf);
     }
 
@@ -35,9 +48,20 @@ public class APIApplication extends ResourceConfig {
     @PreDestroy
     public void stop() {
         LOGGER.debug("Destroying API webapp.");
-        this.irf.shutdown();
+        this.executor.shutdownNow();
     }
 }
+
+// Following the advice from:
+//   http://stackoverflow.com/questions/3745905/what-is-recommended-way-for-spawning-threads-from-a-servlet-in-tomcat
+class SimpleDaemonFactory implements ThreadFactory {
+    public Thread newThread(Runnable r) {
+        final Thread t =new Thread(r);
+        t.setDaemon(true);
+        return t;
+    }
+}
+
 
 /*@Provider
 class JsonMoxyConfigurationContextResolver implements ContextResolver<MoxyJsonConfig> {
