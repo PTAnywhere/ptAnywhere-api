a1f89cf1eefe0ac2c5859ee2c4aab324d71bfdf7
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-08-21 16:47:08 +0100

    Not existing sesssions will be described now with a 410 Gone error instead of the 404. Assumption: they might have existed in the past.

--- src/main/java/uk/ac/open/kmi/forge/ptAnywhere/api/http/exceptions/SessionNotFoundException.java
+++ src/main/java/uk/ac/open/kmi/forge/ptAnywhere/api/http/exceptions/SessionNotFoundException.java
@@ -7,17 +7,19 @@ import javax.ws.rs.core.Response;
 
 // TODO implement ExceptionMapper?
 // http://stackoverflow.com/questions/15185299/jax-rs-jersey-exceptionmappers-user-defined-exception
+// FIXME store past sessions IDs to differentiate between 404 and 410 errors?
 public class SessionNotFoundException extends WebApplicationException {
 
     // BEGIN: used mainly for swagger doc.
-    final public static int status = 404; // Response.Status.NOT_FOUND.getStatusCode() is not a constant for Java
-    final public static String description = "No active session exists with the given id";
+    final public static int status = 410; // Response.Status.GONE.getStatusCode() is not a constant for Java
+    final public static String description = "No active session exists with the given id. " +
+            "This session (and therefore your requested URL) might have existed in the past, but it does not anymore.";
     // END: used mainly for swagger doc.
 
     public SessionNotFoundException(String sessionId, Link... link) {
-        super(Response.status(Response.Status.NOT_FOUND).
+        super(Response.status(Response.Status.GONE).
                 entity(new ErrorBean(
-                        Response.Status.NOT_FOUND.getStatusCode(),
+                        Response.Status.GONE.getStatusCode(),
                         "No session was found with id \"" + sessionId + "\"")).
                 links(link).build()); // is it better to use GONE???
     }
