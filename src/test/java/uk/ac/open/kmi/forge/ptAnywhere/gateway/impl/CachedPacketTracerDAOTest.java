b8aff0cf6b259ed44e8132b02b07166ac89aa907
Aitor Gómez Goiri <aitor.gomez-goiri@open.ac.uk>
2015-10-19 10:57:21 +0100

    Testing cached device search.

--- src/test/java/uk/ac/open/kmi/forge/ptAnywhere/gateway/impl/CachedPacketTracerDAOTest.java
+++ src/test/java/uk/ac/open/kmi/forge/ptAnywhere/gateway/impl/CachedPacketTracerDAOTest.java
@@ -12,8 +12,10 @@ import static org.mockito.Mockito.*;
 
 public class CachedPacketTracerDAOTest {
 
-    final String uuid = "{a9101f6b-ef7c-4372-91c2-9391e94ee233}";
-    final String simpleId = "b8d5exozT9eNsR1udGjbZQ--";
+    final String netUuid = "{a9101f6b-ef7c-4372-91c2-9391e94ee233}";
+    final String netSimpleId = "qRAfa.98Q3KRwpOR6U7iMw--";
+    final String dUuid = "{6fc7797b-1a33-4fd7-8db1-1d6e7468db65}";
+    final String dSimpleId = "b8d5exozT9eNsR1udGjbZQ--";
 
     MemoryCache cache;
     Network network;
@@ -23,7 +25,7 @@ public class CachedPacketTracerDAOTest {
     @Before
     public void setUp() {
         this.network = mock(Network.class);
-        when(this.network.getObjectUUID()).thenReturn(new UUIDImpl(this.uuid));
+        when(this.network.getObjectUUID()).thenReturn(new UUIDImpl(this.netUuid));
         when(this.network.getDeviceCount()).thenReturn(0);
         this.workspace = mock(LogicalWorkspace.class);
         this.cache = new MemoryCache();
@@ -31,9 +33,15 @@ public class CachedPacketTracerDAOTest {
     }
 
     @Test(expected = DeviceNotFoundException.class)
-    public void testGetSimDeviceById() {
-        this.tested.getSimDeviceById(this.simpleId);
+    public void testGetSimDeviceByIdNotCached() {
+        this.tested.getSimDeviceById(this.netSimpleId);
         verify(this.network).getDeviceCount();  // Not found in cache and then looked for it in network
     }
 
+    @Test(expected = DeviceNotFoundException.class)
+    public void testGetSimDeviceByIdCached() {
+        this.cache.add(this.netSimpleId, this.dSimpleId, "device name");
+        this.tested.getSimDeviceById(this.dSimpleId);
+        verify(this.network).getDevice("device name");  // Not found in cache and then looked for it in network
+    }
 }
